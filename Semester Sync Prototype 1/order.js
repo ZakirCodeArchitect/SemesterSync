a88e3580b0874d085c52642c6e87c800577bcbc1
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-03-17 22:54:28 +0500

    Semester Sync prototype 2
    It is basically a user interface to get an idea , how my web application " semester sync " dashboard will look like for managing the semester data.

---
+++ Semester Sync Prototype 1/order.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName : 'Javascript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName : 'CSS Full Course ',
+        productNumber: '97245',
+        paymentStatus: 'Due',
+        status: 'Declined'
+    },
+    {
+        productName : 'Flex-box',
+        productNumber: '36452',
+        paymentStatus: 'Due',
+        status: 'Active'
+    },    
+]
